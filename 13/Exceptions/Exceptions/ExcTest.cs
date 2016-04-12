b56cae769caf9957d9d8a2f9cacfe7b9c90f4a9f
is2139 <shepetov@informstandart.ru>
2016-04-12 16:06:11 +0300

    Òåìà: Ãëàâà 13 - îáðàáîòêà èñêëþ÷åíèé

---
+++ 13/Exceptions/Exceptions/ExcTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class ExcTest
+    {
+        public static void GenException()
+        {
+            int[] nums = new int[4];
+            Console.WriteLine("До генерирования исключения");
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i;
+                Console.WriteLine("nums[{0}]:{1}", i, nums[i]);
+            }
+        }
+    }
+}
---
+++ 13/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[4];
+
+            try
+            {
+                Console.WriteLine("До генерирования ошибки");
+
+                for (int i = 0; i < 10; i++)
+                {
+                    nums[i] = i;
+                    Console.WriteLine("nums[{0}]:{1}", i, nums[i]);
+                }
+
+                Console.WriteLine("Не подлежит выводу");
+            }
+            catch (IndexOutOfRangeException) 
+            {
+                Console.WriteLine("Индекс вышел за границы массива");
+            }
+            Console.WriteLine("После блока перехвата");
+            Console.ReadKey();
+        }
+    }
+
+    class ExcDemo2 
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ExcTest.GenException();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Индекс вышел за границы");
+            }
+            Console.WriteLine("После блока перехвата исключения");
+            Console.ReadKey();
+        }
+    }
+}
