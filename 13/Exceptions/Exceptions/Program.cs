35dc8213a352ea25857ad8b460dfd2929bf95bff
is2139 <shepetov@informstandart.ru>
2016-04-14 15:57:39 +0300

    Òåìà: Çàâåðøèë èçó÷åíèå ãëàâû 13

--- 13/Exceptions/Exceptions/Program.cs
+++ 13/Exceptions/Exceptions/Program.cs
@@ -268,16 +268,29 @@ namespace Classes
     class OrderMatters
     {
         static void Main(string[] args)
-        { 
-            for(int x = 0, x < 3; x++)
+        {
+            for (int x = 0; x < 3; x++)
             {
                 try
                 {
-                    if(x==0) throw new ExceptA("Перехват исключения типа ExceptA");
-                    else if(x==1) throw new ExceptB("Перехват исключения типа ExceptB");
-
+                    if (x == 0) throw new ExceptA("Перехват исключения типа ExceptA");
+                    else if (x == 1) throw new ExceptB("Перехват исключения типа ExceptB");
+                    else throw new Exception();
+                }
+                catch (ExceptB exc)
+                {
+                    Console.WriteLine(exc);
+                }
+                catch (ExceptA exc)
+                {
+                    Console.WriteLine(exc);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc);
                 }
             }
+            Console.ReadKey();
         }
     }
 }
